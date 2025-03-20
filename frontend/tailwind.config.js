cdaffbc1e475e560360a44daf1509cb09ba91373
Izan33 <hugoterrisse33170@gmail.com>
2025-03-20 19:39:28 +0100

    Debug tailwind

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,5 +1,6 @@
-module.exports = {
-  content: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{js,ts,jsx,tsx}"],  
   theme: {
     extend: {},
   },
